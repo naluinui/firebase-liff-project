f49fd5986c9e68bda0caf437410fc5d91e5066aa
Somjintana Korbut <nuisomjin@gmail.com>
2020-03-23 13:00:19 +0700

    Implement create poll view

---
+++ src/views/CreatePoll.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <div class="container">
+      <h1 class="title">Create Pizzanulok Poll</h1>
+      <b-field label="Name">
+          <b-input v-model="name"></b-input>
+      </b-field>
+      <b-button @click="cancel">Cancel</b-button>
+      <b-button type="is-primary" @click="start">Start</b-button>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  beforeCreate() {
+    // TODO: initial LIFF
+  },
+  data () {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    cancel() {
+      this.$buefy.notification.open('Clicked!!')
+      // TODO: close LIFF window
+    },
+    start() {
+      // TODO: create poll in Firebase
+        // TODO: send message to chat room once finish creating poll
+    }
+  }
+}
+</script>
