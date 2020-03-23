2b689e699466fda6ea00c31a9696fb51cf82df45
Somjintana Korbut <nuisomjin@gmail.com>
2020-03-23 16:34:06 +0700

    Improve the UI

--- src/views/PizzaList.vue
+++ src/views/PizzaList.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="section">
-    <div class="container columns is-multiline is-mobile">
+    <div class="container columns is-multiline">
+      <b-loading :is-full-page="true" :active.sync="isLoading" :can-cancel="false"></b-loading>
       <div class="column is-half" v-for="pizza in pizzas" v-bind:key="pizza.id">
         <div class="card">
           <div class="card-image">
@@ -10,13 +11,13 @@
           </div>
           <div class="card-content">
             <div class="content">
-              <p class="title">
+              <p class="title is-2">
                 {{ pizza.name }}
               </p>
               <p class="subtitle">
                 ฿ {{ pizza.price }}
               </p>
-              <p class="subtitle is-7">ส่วนประกอบ: {{ pizza.ingredients.toString() }}</p>
+              <p class="subtitle is-6">ส่วนประกอบ: {{ pizza.ingredients.toString() }}</p>
             </div>
             <b-button type="is-primary is-fullwidth">สั่งเลย</b-button>
           </div>
@@ -31,12 +32,14 @@ const firebase  = require('../firebase.js')
 export default {
   data() {
     return {
-      pizzas: []
+      pizzas: [],
+      isLoading: true
     }
   },
   mounted() {
     const main = this
     firebase.pizzasCollection.get().then(snapshot => {
+      main.isLoading = false
       main.pizzas = snapshot.docs.map ( doc => (
         {id: doc.id, ...doc.data()}
       ))
