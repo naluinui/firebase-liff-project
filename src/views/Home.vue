a2a47f51e4bcd5253d51fe2e20fd88d9fa1314a0
Somjintana Korbut <nuisomjin@gmail.com>
2020-03-25 11:22:28 +0700

    Setup first example

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="section">
+    <div class="container">
+      <h1 class="title"> {{ greetingText }} </h1>
+      <b-field>
+          <b-input v-model="name" placeholder="Tell us your name"></b-input>
+      </b-field>
+      <b-button type="is-primary" @click="enter">Enter</b-button>
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      name: null,
+      greetingText: "Hello! What's your name?"
+    }
+  },
+  methods: {
+    // TODO: 1. Say hello!
+    enter () {
+      if (!this.name) return
+      this.greetingText = "Hello, " + this.name + "!"
+    }
+  },
+  watch: {
+    // TODO: 2. see reactive data
+    name () {
+      if (!this.name) return
+      this.greetingText = "Hello, " + this.name + "!"
+    }
   }
 }
 </script>
